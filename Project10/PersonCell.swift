4a7f5200f3b02ea11dc1d59ae728970b458ccb5b
kabikira <kabikirar2@protonmail.com>
2023-02-23 00:41:30 +0900

    first commit

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by koala panda on 2023/02/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
