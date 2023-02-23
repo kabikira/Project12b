38c9928abd6a8b9c5beb469e325b21f9fa125491
kabikira <kabikirar2@protonmail.com>
2023-02-23 19:21:37 +0900

    add day43

---
+++ Project10/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by koala panda on 2023/02/23.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+
+}
